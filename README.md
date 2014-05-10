9da820ab0f3a7e55ce24e97d221ef3a0f974ea15
Piotr Kowalski <piecioshka@gmail.com>
2014-05-10 14:09:34 +0200

    First version of application

---
+++ README.md
@@ -0,0 +1,15 @@
+# Faktoryzacja - algorytm szybkiego potęgowania modularnego
+
+Mini aplikacja pozwalająca rozłożyć liczbę na czynniki.
+
+## Demo
+
+[http://jsninja.pl/wkr-faktoryzacja/][1]
+
+## License
+
+[The MIT License][2]
+
+
+[1]: http://jsninja.pl/wkr-faktoryzacja/
+[2]: http://piecioshka.mit-license.org/
---
+++ scripts/app.js
@@ -0,0 +1,120 @@
+/**
+ * @author Piotr Kowalski <piecioshka@gmail.com>
+ * @see http://jsninja.pl/wkr-faktoryzacja/
+ * @licence The MIT License {@link http://piecioshka.mit-license.org/}
+ */
+(function (root, factory) {
+    root.App = factory(root._);
+}(this, function (_) {
+    'use strict';
+
+    function App(options) {
+        this.settings = _.extend({}, options);
+        this.initialize();
+    }
+
+    App.prototype = {
+        initialize: function () {
+            var s = this.settings;
+
+            _.bindAll(this, '_enterHandler', '_submitHandler');
+
+            // after click submit do calculation
+            s.submit.on('click', this._submitHandler);
+
+            // print result on any input have been modify
+            s.first.on('keydown', this._enterHandler);
+            s.second.on('keydown', this._enterHandler);
+
+            // set focus on first input
+            s.first.focus();
+        },
+
+        _enterHandler: function (e) {
+            // if `enter`
+            if (e.keyCode === 13) {
+                if (this._checkParams()) {
+                    this._printResult(this._calculate());
+                }
+            } else {
+                this._clearResult();
+            }
+        },
+
+        _submitHandler: function (e) {
+            if (this._checkParams()) {
+                this._printResult(this._calculate());
+            }
+            e.preventDefault();
+        },
+
+        _checkParams: function () {
+            var s = this.settings;
+            var a = s.first.val();
+            var b = s.second.val();
+
+            if (!a.length || !b.length) {
+                alert('Please fill mandatory params');
+                return false;
+            }
+
+            return true;
+        },
+
+        _calculate: function () {
+            var s = this.settings;
+            var a = s.first.val();
+            var b = s.second.val();
+
+            var data = [[a, b]];
+            var r = 0;
+
+            while (a != 0) {
+                r = a % b;
+                a = Math.floor(a / b);
+
+                data.push([a, r]);
+            }
+
+            return data;
+        },
+
+        _printResult: function (result) {
+            var $table = $('<table>').addClass('table table-bordered table-condensed');
+            var $thead = $('<thead>');
+            var $tbody = $('<tbody>');
+
+            // -- header
+
+            var $tr = $('<tr>').addClass('info');
+            _.each(result.shift(), function (cell) {
+                var $th = $('<th>').text(cell);
+                $tr.append($th);
+            });
+            $thead.append($tr);
+            $table.append($thead);
+
+            // --- body
+
+            _.each(result, function (rows) {
+                var $tr = $('<tr>').addClass('active');
+                _.each(rows, function (cell) {
+                    var $td = $('<td>').text(cell);
+                    $tr.append($td);
+                });
+                $tbody.append($tr);
+            });
+
+            $table.append($tbody);
+
+            // show table
+            this.settings.result.removeClass('hidden').html($table);
+        },
+
+        _clearResult: function () {
+            this.settings.result.addClass('hidden');
+        }
+    };
+
+    return App;
+}));
