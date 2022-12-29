03cb925bca188152a9d1d05afa920dc13334d0de
Piotr Kowalski <piecioshka@gmail.com>
2022-12-30 00:11:24 +0100

    Links

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Mini aplikacja pozwalająca rozłożyć liczbę na czynniki.
 
+## Demo 🎉
+
+<https://piecioshka.github.io/wkr-faktoryzacja/>
+
 ## License
 
 [The MIT License](http://piecioshka.mit-license.org/) @ 2014
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 /**
  * @author Piotr Kowalski <piecioshka@gmail.com>
- * @see http://piecioshka.pl/projects/wkr-faktoryzacja/
+ * @see https://piecioshka.github.io/wkr-faktoryzacja/
  * @licence The MIT License {@link http://piecioshka.mit-license.org/}
  */
 (function (root, factory) {
